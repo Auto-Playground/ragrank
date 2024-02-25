2e554a85f62a361fdc79ff5c4b73755119f3a0ab
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-25 13:27:29 +0530

    [feat] setup the base for embedding module

---
+++ src/RAGrank/embedding/base.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import List
+import numpy as np
+
+Embedding = List[float]
+
+
+class SimilarityMode(str, Enum):
+    DEFAULT = "cosine"
+    DOT_PRODUCT = "dot_product"
+    EUCLIDEAN = "euclidean"
+
+def mean_agg(embeddings: List[Embedding]) -> Embedding:
+    return list(np.array(embeddings).mean(axis=0))
+
+def similarity(
+        embedding1: Embedding,
+        embedding2: Embedding,
+        mode: SimilarityMode = SimilarityMode.DEFAULT,
+) -> float:
+    ...
+
+class RagrankBaseEmbegging(ABC):
+
+    @abstractmethod
+    def embed_text(self, text:str) -> Embedding:
+        ...
+
+    def embed_dataset(self, dataset):
+        ...
+
+    def similarity(
+            self,
+            embedding1: Embedding,
+            embedding2: Embedding,
+            mode: SimilarityMode = SimilarityMode.DEFAULT,
+    ) -> float:
+        ...
+
+    async def aembed_text(self, text:str) -> Embedding:
+        ...
+    
+    async def aembed_dataset(self, dataset):
+        ...
+
+    async def asimilarity(self, 
+                          embedding1,
+                          embedding2,
+                          mode: SimilarityMode = SimilarityMode.DEFAULT
+                          ):
+        ...
+
+
+
+
+def embedding_factory():
+    ...
+
