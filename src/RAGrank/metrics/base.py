7058b6f3190b337ae380a9c2ec7c870b5e4963a6
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-25 12:30:20 +0530

    [feat] created the base for metrics module

---
+++ src/RAGrank/metrics/base.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class RagrankBaseMetric(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def evaluate(self):
+        ...
+
+    async def aevaluate(self):
+        ...
+
+    
