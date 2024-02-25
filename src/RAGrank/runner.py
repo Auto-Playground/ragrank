ae2515ff132c466fafe0026055438fdac564417f
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-25 13:23:39 +0530

    [feat] setup the runner and excecuter classes

---
+++ src/RAGrank/runner.py
@@ -0,0 +1,14 @@
+class Runner:
+    def __init__(self) -> None:
+        ...
+
+    def run(self):
+        ...
+
+
+class Excecuter:
+    def __init__(self) -> None:
+        ...
+
+    def execute(self, runner: Runner):
+        ...
