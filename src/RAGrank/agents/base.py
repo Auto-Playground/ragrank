5f6cc914412ee189a469338f6839c7f67bd7e86c
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-25 13:25:33 +0530

    [feat] setup the base for agent

---
+++ src/RAGrank/agents/base.py
@@ -0,0 +1,13 @@
+
+from typing import Any
+
+
+class BaseRagrankAgent:
+    def __init__(self) -> None:
+        ...
+
+    def call(self):
+        ...
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.call()
