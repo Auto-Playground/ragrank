683a4b01e4c83755511e1ba9e395b3bdff3f34b2
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-25 12:41:34 +0530

    added the common constants

---
+++ src/RAGrank/constants.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+KEYFILE: Final[str] = ".ragrank"
+DEFAULT_EMBEDDING_DIMENTION: Final[int] = 64
+DEFAULT_LLM_MODEL: Final[str] = "openai-3.5"
+DEFAULT_METRIC:Final[str] = "relevancy"
