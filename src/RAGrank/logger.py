d6db21311d6b5527c13e8e79c3f96670c57d013e
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-25 13:20:30 +0530

    [feat] setup the logger module

---
+++ src/RAGrank/logger.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import logging
+
+LOGGING_FORMAT = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+LOG_DIR = "logs"
+LOG_FILEPATH = os.path.join(LOG_DIR, "ragrank_logs.log")
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=LOGGING_FORMAT,
+    handlers=[
+        logging.FileHandler(LOG_FILEPATH), 
+        logging.StreamHandler(sys.stdout)
+        ],
+)
+
+logger = logging.getLogger("ragrank_logger")
