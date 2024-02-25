521fd77ba5cb4fef20d6a7a791f670ec382d6d42
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-25 12:40:28 +0530

    [feat] created the base for tracing

---
+++ src/RAGrank/_api.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Union, List
+
+class TraceType(Enum):
+    LLM = "LLM"
+    RETRIEVER = "Retriever"
+    EMBEDDING = "Embedding"
+    TOOL = "Tool"
+    AGENT = "Agent"
+    CHAIN = "Chain"
+
+class TraceStatus(Enum):
+    SUCCESS = "Success"
+    ERROR = "Error"
+
+@dataclass
+class LLMMetadata:
+    model: str
+
+
+@dataclass
+class EmbeddingMetadata:
+    model: str
+
+@dataclass
+class Event:
+    name: str
+
+@dataclass
+class BaseTrace:
+    type: Union[TraceType, str]
+    executionTime: float
+    name: str
+    input: dict
+    output: dict
+    status: TraceStatus
+    traces: List["TraceData"]
+
+
+@dataclass
+class LlmTrace(BaseTrace):
+    input: str
+    llmMetadata: LLMMetadata = None
+
+
+@dataclass
+class EmbeddingTrace(BaseTrace):
+    embeddingMetadata: EmbeddingMetadata
+
+
+@dataclass
+class GenericTrace(BaseTrace):
+    type: str
+
+
+TraceData = Union[LlmTrace, EmbeddingTrace, GenericTrace]
+
+class TraceManager:
+    def __init__(self) -> None:
+        ...
+
+
+
+def trace(event: Event) -> None:
+    ...
