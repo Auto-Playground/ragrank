2c813e9c2b188adf06975dc8100b1eb8b18ece42
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-25 12:29:49 +0530

    [feat] created the base for llm module

---
+++ src/RAGrank/llm/base.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+class RagrankBaseLLM(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def generate_text(self, text:str):
+        ...
+
+    @abstractmethod
+    async def agenerate_text(self, texts):
+        ...
+
+    @abstractmethod
+    def batch(self, batch):
+        ...
+
+    @abstractmethod
+    async def abatch(self, batch):
+        ...
+
+    @abstractmethod
+    def stream(self, text):
+        ...
+
+    @abstractmethod
+    async def stream(self, text):
+        ...
+
+    def get_token_ids(self):
+        ...
+
+    def get_num_tokens(self):
+        ...
