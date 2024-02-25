94bc0285ee28d60b57481dee7445c810bad64bb5
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-25 13:15:24 +0530

    [feat] added the base setup for exceptions module

---
+++ src/RAGrank/exceptions.py
@@ -0,0 +1,12 @@
+class RagRankError(Exception):
+    ...
+
+class ModelInitializationError(RagRankError):
+    def __init__(self, message="Error during model initialization."):
+        self.message = message
+        super().__init__(self.message)
+
+class EvaluationError(RagRankError):
+    def __init__(self, message="Error during evaluation."):
+        self.message = message
+        super().__init__(self.message)
