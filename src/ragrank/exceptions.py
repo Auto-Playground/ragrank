abb3e9cbce45a5425dbabdbcec524a2ea0596604
Izam Mohammed <izamdeveloper1@gmail.com>
2024-03-01 19:24:43 +0530

    cleaned project for get rid of linting errors

--- src/ragrank/exceptions.py
+++ src/ragrank/exceptions.py
@@ -1,13 +1,9 @@
-class RagRankError(Exception): ...
-
-
-class ModelInitializationError(RagRankError):
-    def __init__(self, message="Error during model initialization."):
-        self.message = message
-        super().__init__(self.message)
+class RagRankError(Exception): 
+    """Exception class for ragrank"""
 
 
 class EvaluationError(RagRankError):
+    """Evaluation Exception for ragrank"""
     def __init__(self, message="Error during evaluation."):
         self.message = message
         super().__init__(self.message)
