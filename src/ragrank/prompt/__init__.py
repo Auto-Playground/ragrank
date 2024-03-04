51bdc892f8a21daa8709a099410c74675e228848
Izam Mohammed <izamdeveloper1@gmail.com>
2024-03-04 22:38:20 +0530

    [feat] added the prompt class

---
+++ src/ragrank/prompt/__init__.py
@@ -0,0 +1,5 @@
+""" "The prompt module for ragrank"""
+
+from ragrank.prompt.base import Prompt
+
+__all__ = ["Prompt"]
---
+++ src/ragrank/prompt/base.py
@@ -0,0 +1,65 @@
+"""base fo the the prompt module"""
+
+from typing import Dict, List, Optional
+
+from ragrank.bridge.pydantic import BaseModel, model_validator
+
+Example = Dict[str, str]
+
+
+class Prompt(BaseModel):
+    """Prompt for the LLM"""
+
+    name: str
+    instructions: str
+    examples: List[Example] = []
+    input_keys: List[str]
+    output_key: str
+
+    @model_validator(mode="after")
+    def validate_prompt(self) -> "Prompt":
+        """Pydantic validation function"""
+
+        if not self.examples:
+            raise ValueError("Examples Cannot be empty")
+        if not self.input_keys:
+            raise ValueError("Input keys Cannot be empty")
+        return self
+
+    def to_string(self) -> str:
+        """Convert the Prompt to a string"""
+
+        prompt_str = self.name + "\n\n"
+        prompt_str += self.instructions + "\n"
+
+        for example in self.examples:
+            for key, value in example.items():
+                prompt_str += f"\n{key}: {value}"
+            prompt_str += "\n"
+
+        if self.input_keys:
+            prompt_str += "".join(
+                f"\n{key}: {{{key}}}" for key in self.input_keys
+            )
+
+        if self.output_key:
+            prompt_str += f"\n{self.output_key}\n"
+
+        return prompt_str
+
+    def get_examples(self, example_no: Optional[int] = None) -> str:
+        """Return the examples in the class"""
+
+        if example_no is None:
+            return self.examples
+        if example_no > len(self.examples):
+            raise IndexError("Example Number is out of range")
+        return self.examples[0:example_no]
+
+    def save(self) -> None:
+        """Save the object locally"""
+        raise NotImplementedError
+
+    def load(self) -> None:
+        """Load the saved object from the system"""
+        raise NotImplementedError
