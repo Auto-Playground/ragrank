7c7d79aa875bf93fd6b11eb1c8c9ed5f4c452daf
Izam Mohammed <izamdeveloper1@gmail.com>
2024-03-04 22:37:42 +0530

    [test] added more test cases for dataset

--- tests/unit_tests/test_dataset.py
+++ tests/unit_tests/test_dataset.py
@@ -1,25 +1,58 @@
 import pytest
 from ragrank.dataset import Dataset, from_dict
-from ragrank.logger import logger
+from ragrank.exceptions import ValidationError
 
 
 @pytest.fixture
-def mock_dataset_dict():
-    data = {
-        "question": "Who is the precident of America ?",
-        "context": ["The president of America is Trump"],
-        "response": "Trump",
+def valid_dict_data():
+    """Fixture for valid dictionary data."""
+    return {
+        "question": "What is the capital of France?",
+        "context": [
+            "Paris is the capital of France.",
+            "It is located in Europe.",
+        ],
+        "response": "The capital of France is Paris.",
     }
-    return data
 
 
-def test_dataset(mock_dataset_dict):
-    dataset = Dataset(**mock_dataset_dict)
-    logger.info(f"{dataset}")
-    assert True
+@pytest.fixture
+def invalid_dict_data():
+    """Fixture for invalid dictionary data."""
+    return {
+        "question": "What is the capital of France?",
+        "context": "Paris is the capital of France.",
+        "response": "The capital of France is Paris.",
+    }
 
 
-def test_from_dataset(mock_dataset_dict):
-    dataset = from_dict(mock_dataset_dict)
-    logger.info(f"{dataset}")
-    assert True
+def test_from_dict_valid(valid_dict_data):
+    """Test creation of a Dataset object from a valid dictionary."""
+
+    dataset = from_dict(valid_dict_data)
+    assert isinstance(dataset, Dataset)
+    assert dataset.question == valid_dict_data["question"]
+    assert dataset.context == valid_dict_data["context"]
+    assert dataset.response == valid_dict_data["response"]
+
+
+def test_from_dict_invalid(invalid_dict_data):
+    """Test creation of a Dataset object from an invalid dictionary."""
+
+    with pytest.raises(ValidationError):
+        data = from_dict(invalid_dict_data)
+
+
+def test_dataset_creation_valid(valid_dict_data):
+    """Test creation of a Dataset object with valid data."""
+    dataset = Dataset(**valid_dict_data)
+    assert isinstance(dataset, Dataset)
+    assert dataset.question == valid_dict_data["question"]
+    assert dataset.context == valid_dict_data["context"]
+    assert dataset.response == valid_dict_data["response"]
+
+
+def test_dataset_creation_invalid_context(invalid_dict_data):
+    """Test creation of a Dataset object with invalid context."""
+    with pytest.raises(ValueError):
+        Dataset(**invalid_dict_data)
