290ab3bc4b42bb6ee9bd4dbd67bce0570c00ca3b
Izam Mohammed <izamdeveloper1@gmail.com>
2024-03-13 14:11:03 +0530

    feat: completed the dataset and datanode methods

--- src/ragrank/dataset/reader.py
+++ src/ragrank/dataset/reader.py
@@ -2,17 +2,37 @@
 
 from __future__ import annotations
 
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from ragrank.bridge.pydantic import ValidationError
-from ragrank.dataset import Dataset
+from ragrank.dataset import DataNode, Dataset
 from ragrank.exceptions import ValidationError as RagrankValidationError
 
+DATANODE_TYPE = Dict[str, Union[List[str], str]]
+DATASET_TYPE = Dict[str, Union[List[str], List[List[str]]]]
 
-def from_dict(data: Dict[str, List[str] | str]) -> Dataset:
-    """create a dataset object from dictionary"""
+
+def from_dict(
+    data: Union[DATANODE_TYPE, DATASET_TYPE],
+) -> Union[Dataset, DataNode]:
+    """
+    Create a Dataset or DataNode object from a dictionary representation.
+
+    Args:
+        data (Union[DATANODE_TYPE, DATASET_TYPE]): The dictionary containing
+            the data representation.
+
+    Returns:
+        Union[Dataset, DataNode]: Either a Dataset or DataNode object.
+
+    Raises:
+        RagrankValidationError: If there is an issue with the
+            schema validation.
+    """
 
     try:
+        if any(isinstance(i, str) for i in data.values()):
+            return DataNode(**data)
         return Dataset(**data)
     except ValidationError as e:
-        raise RagrankValidationError(str(e)) from None
+        raise RagrankValidationError(f"Schema validation failed: {e}") from e
